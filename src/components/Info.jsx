b09d62bf2fd460f51cba690d76e79b4ba4d4bf03
SairamNomula <sairamnomula14@gmail.com>
2021-06-24 21:12:27 +0530

    Updt

---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Greengram</h1>
+      <p>Best in rainy season</p>
+    </div>
+  );
+}
+
+export default Info;
